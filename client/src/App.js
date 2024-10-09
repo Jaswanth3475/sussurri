7dced16cc7b7848c2e35c149f76a282ce3f6b1c0
Jaswanth <k.jaswanth.goud@gmail.com>
2024-10-09 20:16:14 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CallAwsStreaming from './CallAwsStreaming';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CallAwsStreaming/>
+    </div>
+  );
+}
+
+export default App;
